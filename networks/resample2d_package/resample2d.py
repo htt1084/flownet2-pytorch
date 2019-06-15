da954a83dccd8993339a47bb88b98d007fbbbcf5
Ubuntu <ubuntu@ip-172-31-55-243.ec2.internal>
2019-06-15 07:22:34 +0000

    updated networks

--- networks/resample2d_package/resample2d.py
+++ networks/resample2d_package/resample2d.py
@@ -5,25 +5,23 @@ import resample2d_cuda
 class Resample2dFunction(Function):
 
     @staticmethod
-    def forward(ctx, input1, input2, kernel_size=1, bilinear= True):
+    def forward(ctx, input1, input2, kernel_size=1):
         assert input1.is_contiguous()
         assert input2.is_contiguous()
 
         ctx.save_for_backward(input1, input2)
         ctx.kernel_size = kernel_size
-        ctx.bilinear = bilinear
 
         _, d, _, _ = input1.size()
         b, _, h, w = input2.size()
         output = input1.new(b, d, h, w).zero_()
 
-        resample2d_cuda.forward(input1, input2, output, kernel_size, bilinear)
+        resample2d_cuda.forward(input1, input2, output, kernel_size)
 
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        grad_output = grad_output.contiguous()
         assert grad_output.is_contiguous()
 
         input1, input2 = ctx.saved_tensors
@@ -39,11 +37,10 @@ class Resample2dFunction(Function):
 
 class Resample2d(Module):
 
-    def __init__(self, kernel_size=1, bilinear = True):
+    def __init__(self, kernel_size=1):
         super(Resample2d, self).__init__()
         self.kernel_size = kernel_size
-        self.bilinear = bilinear
 
     def forward(self, input1, input2):
         input1_c = input1.contiguous()
-        return Resample2dFunction.apply(input1_c, input2, self.kernel_size, self.bilinear)
+        return Resample2dFunction.apply(input1_c, input2, self.kernel_size)
--- networks/resample2d_package/resample2d_kernel.cuh
+++ networks/resample2d_package/resample2d_kernel.cuh
@@ -6,8 +6,7 @@ void resample2d_kernel_forward(
     at::Tensor& input1,
     at::Tensor& input2,
     at::Tensor& output,
-    int kernel_size,
-    bool bilinear);
+    int kernel_size);
 
 void resample2d_kernel_backward(
     at::Tensor& input1,
